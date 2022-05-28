7796f4f33af0717cb5b4c53b3275214d72d50524
Daniel_Pereira_Monteiro <Danielpereira227@gmail.com>
2022-05-28 11:16:38 -0300

    ramificaçoes no pai

--- teste_fork.c
+++ teste_fork.c
@@ -1,13 +1,29 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/types.h>
 #include<stdlib.h>
-void main(){
-
+int main(){
+printf("\nAAAAAAAAAAAa\n");
  pid_t x;
+ //pid_t y;
  x=fork();
- printf("%d",x);
+ //y=fork();
+ if(x!=0){
+      printf("\nfilho X %d",x);
+      //printf("\nfilho Y %d",y);
+      printf("\npai X %d",getpid());
+
+ }
+ //if(y!=0){
+   // printf("\nfilho Y %d",y);
+     /// printf("\npai%d Y",getpid());
 
 
+ //}
+
+
+
+ return 0;
 
     
 }
