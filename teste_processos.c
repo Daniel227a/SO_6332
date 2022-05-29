30a006bcf5db0ca64447181a07e9993778bb416f
Daniel_Pereira_Monteiro <Danielpereira227@gmail.com>
2022-05-29 11:11:31 -0300

    turnaround_time

--- teste_processos.c
+++ teste_processos.c
@@ -30,6 +30,63 @@ void process_init(Process p[], int len)
     }
 }
 
+void fcfs_print_gantt_chart(Process *p, int len)
+{
+    int i, j;
+    printf("\t ");
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < p[i].burst; j++)
+            printf("--");
+
+        printf(" ");
+    }
+
+    printf("\n\t|");
+
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < p[i].burst - 1; j++)
+            printf(" ");
+
+        printf("%s", p[i].id);
+
+        for (j = 0; j < p[i].burst - 1; j++)
+            printf(" ");
+
+        printf("|");
+    }
+
+    printf("\n\t ");
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < p[i].burst; j++)
+            printf("--");
+
+        printf(" ");
+    }
+
+    printf("\n\t");
+
+
+    printf("0");
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < p[i].burst; j++)
+            printf("  ");
+
+        if (p[i].return_time > 9)
+            printf("\b");
+
+        printf("%d", p[i].return_time);
+
+    }
+
+    printf("\n");
+}
 void main(){
     Process *process;
 
@@ -41,5 +98,5 @@ void main(){
 
     }
 
-
+    fcfs_print_gantt_chart(process,5);
 }
