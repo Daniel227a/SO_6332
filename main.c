ecea20361ecfbf1a0dba70d7256b06640244ecfc
Daniel_Pereira_Monteiro <Danielpereira227@gmail.com>
2022-05-30 19:20:56 -0300

    return time total_FIFO

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include "CpuSchedulingAlgorithms.h"
+
+
 void main(){
 
 
         int process_count = 0;
 
     int i = 0;
+    int j;
 
 
     Quantum quantum;
 
     Process *process;
-
+    Process *temp;
     FILE *fp = fopen("process.txt", "r");
 
 
@@ -22,16 +25,17 @@ void main(){
     fscanf(fp, " %d", &process_count);
 
     process = (Process *) malloc(sizeof(Process) * process_count);
-
-
+    temp=(Process *) malloc(sizeof(Process) * process_count);
     while (i < process_count) {
         fscanf(fp, "%s %d %d %d",
                process[i].id, &process[i].arrive_time, &process[i].burst, &process[i].priority);
 
         i++;
-
     }
     i=0;
+  
+    
+
     for (i=0;i<process_count;i++){
 
         printf(" id %s",process[i].id);
@@ -41,4 +45,30 @@ void main(){
         printf("\n");
 
     }
+
+    int time_cont;
+    time_cont=0;
+    for(i=0;i<process_count;i++){
+        for(j=i;j>=0;j--){
+             time_cont=time_cont+process[j].burst;
+             time_cont=time_cont+process[j].arrive_time;
+            
+        }
+    process[i].return_time=time_cont;
+    time_cont=0;
+    printf("\n");
+    }
+    
+    for (i=0;i<process_count;i++){
+
+        printf(" id %s",process[i].id);
+        printf(" tempo de espera (waiting_time): %d",process[i].arrive_time);
+        printf(" é o tempo total gasto pelo processo para sua execução na CPU (brust): %d",process[i].burst);
+        printf(" priridade : %d",process[i].priority);
+        printf(" return time : %d",process[i].return_time);
+        printf("\n");
+
+    }
+
+
 }
