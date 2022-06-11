3e214a405943cccd61625123c91b160297eacbeb
Daniel_Pereira_Monteiro <Danielpereira227@gmail.com>
2022-06-11 01:14:44 -0300

    arrive_time_sort

--- funçoes.h
+++ funçoes.h
@@ -18,3 +18,35 @@ void lerTxt(){
 
 }
 
+void arrive_time_sort(Process *process,int i, int process_count){
+
+    int cont=0;
+    int i,j;
+    Process temp_process;
+    temp=(Process *) malloc(sizeof(Process) * process_count);
+    for(j=i+1;j<process_cont;j++){
+        if((process[i].burst+process[i].arrive_time)>process[j].arrive_time){
+
+            temp[cont]=process[j].arrive_time;
+            cont++;
+        }
+
+    }
+    for(i=0;i<cont-1;i++){
+        for(j=i+1;j<cont;j++){
+            if(process[i]>process[j]){
+                temp_process=temp[i];
+                temp[i]=temp[j];
+                temp[j]=temp_process;
+
+            }
+          
+        }
+
+
+    }
+
+
+
+
+}
